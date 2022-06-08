4ca4fdfcb982c7acb7d8ecbc18cba7976a85699b
gingerFury7 <slavka2000@mail.ru>
2022-06-08 16:48:22 +0300

    lang change mobile

--- src/components/header.js
+++ src/components/header.js
@@ -23,23 +23,23 @@ const Header = () => {
     return (
         <header className="header">
             <HeaderContainer>
-                <Link to="/" className="mobile-logo" onClick={() => setIsChecked(false)}>
+                <Link to="/" className="mobile-logo" >
                     <LogoMobile src="/Logo/logo.svg" alt="logo"></LogoMobile>
                 </Link>
                 <HeaderContent>
                     <li>
-                        <Link to="/" className="mobile-logo" onClick={() => setIsChecked(false)}>
+                        <Link to="/" className="mobile-logo">
                             <Logo src="/Logo/logo.svg" alt="logo"></Logo>
                         </Link>
                     </li>
                     <li>
-                        <Link to="/contacts" className="tertiaryButtonText" onClick={() => setIsChecked(false)}>{t('contacts')}</Link>
+                        <Link to="/contacts" className="tertiaryButtonText">{t('contacts')}</Link>
                     </li>
                     <li>
-                        <Link to="/calculation" className="tertiaryButtonText" onClick={() => setIsChecked(false)}>{t('calculator')}</Link>
+                        <Link to="/calculation" className="tertiaryButtonText">{t('calculator')}</Link>
                     </li>
                     <li style={{ border: "none" }}>
-                        <Link to="/about" className="tertiaryButtonText" onClick={() => setIsChecked(false)}>{t('about')}</Link>
+                        <Link to="/about" className="tertiaryButtonText" >{t('about')}</Link>
                     </li>
                     <li aria-haspopup="true" style={{ marginTop: "17px" }}>
                         <span>{languages.find((l) => l.code == currentLanguageCode).name}</span> <i class="arrow down"></i>
@@ -68,36 +68,33 @@ const Header = () => {
                         </ul>
                     </li>
                 </HeaderContent>
-                <SelectLanguageMobile>
-                    <li>
-                        <span>
-                            <LangIcon src="https://res.cloudinary.com/dxjubrqnd/image/upload/v1654690002/daark/language-svgrepo-com_1_m9lhvo.svg" />
-                        </span>
-                        <ul name="languages" onClick={(e) => {
-                            i18next.changeLanguage(e.target.value)
-                        }}>
-                            {
-                                languages.map(({ code, name, country_code }) => (
-                                    <li key={country_code}>
-                                        <a
-                                            href={`#${code}`}
-                                            onClick={() => {
-                                                i18next.changeLanguage(code)
-                                            }}
-                                        >
-                                            <span
-                                                style={{
-                                                    opacity: currentLanguageCode === code ? 0.5 : 1,
-                                                }}
-                                            ></span>
-                                            {name}
-                                        </a>
-                                    </li>
-                                ))
-                            }
-                        </ul>
-                    </li>
-                </SelectLanguageMobile>
+                <MenuToggle id="menu-toggle" checked={isChecked} type="checkbox" onChange={() => setIsChecked(!isChecked)}/>
+                <MenuButtonContainer class='menu-button-container' for="menu-toggle">
+                    <div class='menu-button'>
+                        <LangIcon src="https://res.cloudinary.com/dxjubrqnd/image/upload/v1654690002/daark/language-svgrepo-com_1_m9lhvo.svg" />
+                    </div>
+                </MenuButtonContainer>
+                <MobileMenu>
+                    {
+                        languages.map((e) => (
+                            <li key={e.code}>
+                                <a
+                                    href={`#${e.code}`}
+                                    onClick={() => {
+                                        i18next.changeLanguage(e.code);
+                                        setIsChecked(false);
+                                    }}
+                                >
+                                    <span
+                                        style={{
+                                            opacity: currentLanguageCode === e.code ? 0.5 : 1,
+                                        }}
+                                    ></span>
+                                    {e.name}
+                                </a></li>
+                        ))
+                    }
+                </MobileMenu>
             </HeaderContainer>
             <Outlet />
         </header>
@@ -222,104 +219,78 @@ const LogoMobile = styled.img`
     }
 `
 
-const SelectLanguageMobile = styled.ul`
-    border: none;
-    height: fit-content;
-    font-family: 'Inter';
-    font-size: 18px;
-    line-height: 23px;
-    display: none;
-    list-style: none;
-
-    @media screen and (max-width: 767px){
-        display: block;
-        position: absolute;
-        right: 24px;
-        left: auto;
-        text-decoration: none;
-        padding-left: unset;
-        margin: 0;
-
-        li {
-            text-decoration: none;
-            transition-duration: 0.5s;
-            font-family: 'Inter';
-            font-size: 24px;
-            line-height: 23px;
-            float: left;
-            position: relative;
-        }
-
-        li > span {
-            align-self: center;
-        }
-
-        li:hover, li:focus-within {
-            cursor: pointer
-        }
-
-        li ul {
-            visibility: hidden;
-            opacity: 0;
-            position: absolute;
-            transition: all 0.5s ease;
-            right: 0;
-            display: none;
-            margin-top: 7px;
-            list-style: none;
-            padding: 0.5em 0px;
-            padding-left: unset;
-            z-index: 101;
-            background-color: white;
-            box-shadow: 2px 8px 24px 1px rgba(0, 0, 0, 0.2);
-            border-radius: 15px;
-        }
-
-        li:hover > ul,
-        li:focus-within > ul,
-        li ul:hover,
-        li ul:focus {
-            visibility: visible;
-            opacity: 1;
-            display: block;
-        }
-
-        li ul li {
-            clear: both;
-            width: 50%;
-            padding: 1em 2em;
-            text-align: center;
-        }
-
-        li ul li a {
-            width: 100%;
-            height: 100%;
-        }
-    }
-
-    @media (hover: none) and (pointer: fine){
-        li:hover > ul,
-        li:focus-within > ul,
-        li ul:hover,
-        li ul:focus {
-            visibility: hidden;
-            opacity: 1;
-            display: none;
-        }
-    }
-
-`
-
-const SelectLanguageOption = styled.li`
-    background-color: white;
-    clear: both;
-    width: 100%;
-    padding: 0.3em;
-    padding-left: unset;
-`
 const LangIcon = styled.img`
     width: 40px;
     height: 40px;
+`
+
+const MobileMenu = styled.ul`
+    display: none;
+    flex-direction: row;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    font-family: 'Inter';
+    font-size: 24px;
+    line-height: 30px;
+    background-color: white;
+
+
+    & > li {
+        margin: 0 1em;
+        overflow: hidden;
+    }   
+
+    @media screen and (max-width: 767px) {
+        display: flex;
+        position: absolute;
+        top: 0;
+        margin-top: 60px;
+        right: 24px;
+        flex-direction: column;
+        width: 10em;
+        justify-content: center;
+        align-items: center;
+        border-radius: 15px;
+    }
+`
+
+const MenuButtonContainer = styled.label`
+    display: none;
+    height: 100%;
+    width: 40px;
+    cursor: pointer;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    @media screen and (max-width: 767px) {
+        display: flex;
+        position: absolute;
+        right: 24px;
+    }
+
+`
+const MenuToggle = styled.input`
+    display: none;
+
+    & ~ ${MobileMenu} li {
+        height: 0;
+        margin: 0;
+        padding: 0;
+        border: 0;
+    }
+
+    &:checked ~ ${MobileMenu} {
+        height: 8em;
+        box-shadow: 2px 8px 24px 1px rgba(0, 0, 0, 0.2);
+    }
+
+    &:checked ~ ${MobileMenu} li {
+        height: 1em;
+        padding: 1.5em;
+    }
+
 `
 
 export default Header
